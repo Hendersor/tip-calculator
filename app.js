6ff21f491a1a4a862d23e200ad1c1fdd6c1d4c58
Francisco <montejof1998@gmail.com>
2022-01-13 15:36:22 -0800

    Getting the values

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
 const billContainer = document.getElementById("billContainer");
+const tipList = document.querySelectorAll('.tipC');
+const peopleContainer = document.getElementById('peopleContainer')
 
-console.log(billContainer);
+let billValue = 0;
+let tipValue = 0;
+let peopleValue = 0;
+
+billContainer.addEventListener('input', getBillValue);
+
+function getBillValue(){
+     billValue = billContainer.value
+}
+
+tipList.forEach(i => {
+     i.addEventListener('click', getTipValue);
+})
+
+function getTipValue(e){
+     tipValue = parseInt(e.target.innerText);
+}
+
+
+peopleContainer.addEventListener('input', getPeopleValue);
+
+function getPeopleValue(){
+     peopleValue = peopleContainer.value;
+     console.log(peopleValue)
+}

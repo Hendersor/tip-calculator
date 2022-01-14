38099b73496733401907491df152c1e011cb19c9
Francisco <montejof1998@gmail.com>
2022-01-13 19:26:33 -0800

    added some details to the js

--- app.js
+++ app.js
@@ -1,29 +1,56 @@
 const billContainer = document.getElementById("billContainer");
 const tipList = document.querySelectorAll('.tipC');
 const peopleContainer = document.getElementById('peopleContainer')
+const tipContainer = document.getElementById('tipValue');
+const customTip = document.getElementById('tipI');
 
-let billValue = 0;
-let tipValue = 0;
-let peopleValue = 0;
+let billValue;
+let tipValue;
+let peopleValue;
+let customTipValue;
 
 billContainer.addEventListener('input', getBillValue);
-
 function getBillValue(){
      billValue = billContainer.value
+     if(billValue > 0){
+          getTip();
+     }
 }
+
 
 tipList.forEach(i => {
      i.addEventListener('click', getTipValue);
+     
 })
-
 function getTipValue(e){
      tipValue = parseInt(e.target.innerText);
 }
 
+customTip.addEventListener('input', getCustomTip);
+function getCustomTip(){
+     customTipValue = customTip.value;
+     if(customTipValue > 0 ){
+          getTip2();
+     }
+}
+
 
 peopleContainer.addEventListener('input', getPeopleValue);
-
 function getPeopleValue(){
      peopleValue = peopleContainer.value;
-     console.log(peopleValue)
+     if(peopleValue >= 1){
+          getTip()
+     }
+}
+
+
+function getTip(){
+     tipValue = (billValue / peopleValue) * (tipValue / 100)
+     tipContainer.innerText = tipValue;
+}
+
+
+function getTip2(){
+     tipValue = (billValue / peopleValue) * (customTipValue / 100)
+     tipContainer.innerText = tipValue;
 }

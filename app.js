78bdf468ab86283b12adf14b9cb77b1109ba104d
Francisco <montejof1998@gmail.com>
2022-01-14 17:52:51 -0800

    Tip calculator done

--- app.js
+++ app.js
@@ -4,17 +4,14 @@ const peopleContainer = document.getElementById('peopleContainer')
 const tipContainer = document.getElementById('tipValue');
 const customTip = document.getElementById('tipI');
 const totalBill = document.getElementById('totalBill');
+const resetBtn = document.getElementById('idReset');
 
-let billValue;
-let tipValue;
-let finalTipValue;
-let peopleValue;
-let customTipValue;
-let finalBill;
+let billValue, tipValue, finalTipValue, peopleValue, customTipValue, finalBill;
+
 
 billContainer.addEventListener('input', getBillValue);
 function getBillValue(){
-     billValue = billContainer.value
+     billValue = parseFloat(billContainer.value);
      if(tipValue > 0 && peopleValue > 0 && billValue > 0){
           getTip();
      }
@@ -25,7 +22,6 @@ tipList.forEach(i => {
      
 })
 function getTipValue(e){
-
      tipList.forEach(i => {
           i.classList.remove('on')
           if(e.target.classList.innerText == i.classList.innerText){
@@ -40,23 +36,29 @@ function getTipValue(e){
           totalBill.innerText = "$00.0";
           tipContainer.innerText = "$00.0";
      }
+
+     customTip.value = '';
 }
+
+
 
 customTip.addEventListener('input', getCustomTip);
 function getCustomTip(){
-     customTipValue = customTip.value;
+     customTipValue = customTip.value / 100;
      parseFloat(customTipValue);
+     getTip2();
 }
 
 
 peopleContainer.addEventListener('input', getPeopleValue);
 function getPeopleValue(){
      peopleValue = peopleContainer.value;
-     if(tipValue > 0 && peopleValue > 0 && billValue > 0){
+     if(tipValue > 0 && peopleValue > 0 && billValue > 0 && customTipValue !== 0){
           getTip();
+     }else if(customTipValue > 0){
+          getTip2();
      }else{
-          totalBill.innerText = "$00.0";
-          tipContainer.innerText = "$00.0";
+          getTip();
      }
 }
 
@@ -70,8 +72,29 @@ function getTip(){
 
 
 function getTip2(){
+     tipList.forEach(i => {
+          i.classList.remove('on');
+     })
+
+     console.log(billValue, customTipValue, peopleValue);
      finalTipValue = (billValue / peopleValue) * customTipValue;
      finalBill = (billValue / peopleValue) + finalTipValue; 
      tipContainer.innerText = parseFloat(finalTipValue).toFixed(2);
      totalBill.innerText = parseFloat(finalBill).toFixed(2);
+     
+}
+
+
+idReset.addEventListener('click', resetAll);
+
+function resetAll(){
+     billValue = 0;
+     peopleValue = 0;
+     finalTipValue = 0;
+     billContainer.value = '';
+     tipContainer.value = '';
+     peopleContainer.value = '';
+     customTip.value = '';
+     totalBill.innerText = "$00.0";
+     tipContainer.innerText = "$00.0";
 }
